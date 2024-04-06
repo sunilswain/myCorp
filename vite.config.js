1e049213ad5b290ba9f98f60ff360bc426386c23
Sunil Swain <sunilswain5555@gmail.com>
2024-04-07 02:30:40 +0530

    commit 3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/myCorp/', // Adjust this to match your GitHub repository name
 })
